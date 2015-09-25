da4275184296ab2cd9e80965f0b2d645be5d5f38
maegann <maegann32@utexas.edu>
2015-09-25 14:01:47 -0500

    Create dupKey.py

---
+++ dupKey.py
@@ -0,0 +1,10 @@
+rep = ""
+with open("uniprot_dup.fasta.clstr") as inFile:
+    with open("duplicates_key.txt", "w") as outFile:
+        for line in inFile:
+            if line.startswith("0"):
+                rep = line.split()
+            else:
+                if ">sp" in line:
+                    dup = line.split()
+                    outFile.write(rep[2] + "\t" + dup[2] + "\n")
