dfd9863a2e61957a9e596f1781194726a9f94a1b
maegann <maegann32@utexas.edu>
2015-09-29 15:37:52 -0500

    Create findUnique.py

---
+++ findUnique.py
@@ -0,0 +1,22 @@
+# Find all with a specific about of unique
+# can then sort and inspect to see if canonical and isoform of a certain uniprot id are both present
+
+import os
+directories = os.listdir(os.getcwd())
+outFile = open ('MS_uniques.txt', 'w')
+for i in directories:
+    if '.combined-Results' in i:
+        try:
+            inFile = open (i + '/msblender.prot_count_mFDRpsm001', 'r')
+            for line in inFile:
+                if (not line.startswith('#')) and (not line.startswith("CONTAMINANT")):
+                    spLine = line.split()
+                    if spLine[3] > '1':
+                        #outLine = line.replace(*'sp|', 'sp|'
+                       # outFile.write(i + '\t' + line)
+                       outFile.write(line)
+            inFile.close()
+        except IOError:
+            print "Error: File doesn't exist in", i
+
+outFile.close()
