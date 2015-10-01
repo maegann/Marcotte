d8b96bcdd6525ce24ed70ced9e0f8b21cbe85da9
maegann <maegann32@utexas.edu>
2015-10-01 17:24:06 -0500

    Create findMulti.py

---
+++ findMulti.py
@@ -0,0 +1,23 @@
+#Creates a file with a list of all the proteins in alphabetical order
+#Takes that file and creates a file of proteins that have isoforms
+with open("HS_CB660_uniprot.prot_count_mFDRpsm001.unique_rmzero") as inFile:
+    with open("protein_list", "w") as outFile:
+        for line in inFile:
+            if not(line.startswith('CONTAMINANT') or line.startswith('#')):
+                sline = line.split()
+                outFile.write(sline[0] +'\n')
+protein = ''
+possible = ''
+newProtein = True
+with open("protein_list") as inFile2:
+    with open("prots_w_isoforms", "w") as outFile2:
+        for line in inFile2:
+            possible = line.split('-')            
+            if not(protein == '') and possible[0] == protein[0]:
+                if newProtein == True:
+                      outFile2.write('-'.join(protein))
+                      newProtein = False
+                 outFile2.write(line)
+              else:
+                  protein = line.split('-')
+                  newProtein = True
