8955130e56f7be3a25473652a341b1990adcfe21
maegann <maegann32@utexas.edu>
2015-10-05 15:54:06 -0500

    Finds cases where multi isoforms found in elution

--- findMulti.py
+++ findMulti.py
@@ -1,23 +1,26 @@
 #Creates a file with a list of all the proteins in alphabetical order
 #Takes that file and creates a file of proteins that have isoforms.
-with open("HS_Q166_uniprot.prot_count_mFDRpsm001.unique_rmzero") as inFile:
+with open("test.txt") as inFile:
     with open("unique_proteins.txt", "w") as outFile:
         for line in inFile:
             if not(line.startswith('CONTAMINANT') or line.startswith('#')):
                 sline = line.split()
-                outFile.write(sline[0] +'\n')
+                outFile.write(sline[0]+'\n')
 protein = ''
 possible = ''
-newProtein = True
+newProtein = False
 with open("unique_proteins.txt") as inFile2:
     with open("multi_isoforms.txt", "w") as outFile2:
         for line in inFile2:
-            possible = line.split('-')
-            if not(protein == '') and possible[0] == protein[0]:
+            temp = line.rstrip('\n')
+            possible = temp.split('-')
+            if protein != '' and possible[0] == protein[0]:
                 if newProtein == True:
-                      outFile2.write('-'.join(protein))
-                      newProtein = False
-                      outFile2.write(line)
+                    outFile2.write('-'.join(protein)+'\n')
+                    newProtein = False
+                    outFile2.write('-'.join(possible)+'\n')
+                else:
+                    outFile2.write('-'.join(possible)+'\n')
             else:
-                protein = line.split('-')
-                newProtein = True
+                 protein = possible
+                 newProtein = True
