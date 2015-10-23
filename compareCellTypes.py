713a1540b86697dc1c1883db7c39959fb526f205
maegann <maegann32@utexas.edu>
2015-10-23 14:03:24 -0500

    Update compareCellTypes.py

--- compareCellTypes.py
+++ compareCellTypes.py
@@ -1,6 +1,6 @@
 # Compare 2 files that list all of the unique proteins found to see which ones are cell type specific
 inCell1 = open('HS_CB660_proteins.txt')
-outCell1 = open('HS_CB660_only.txt', 'w')
+outCell1 = open('cell1_only.txt', 'w')
 for line1 in inCell1:
   unique = True
   inCell2 = open('HS_G166_1105_proteins.txt')
@@ -14,7 +14,7 @@ outCell1.close()
 inCell1.close()
 
 inCell2 = open('HS_G166_1105_proteins.txt')
-outCell2 = open('HS_G166_1105_only.txt', 'w')
+outCell2 = open('cell2_only.txt', 'w')
 for line2 in inCell2:
   unique = True
   inCell1 = open('HS_CB660_proteins.txt')
@@ -31,15 +31,14 @@ inCell1.close()
 inCell2.close()
 outCell2.close()
 
-inCell1 = open('HS_CB660_only.txt')
-inCell2 = open('HS_G166_1105_only.txt')
+inCell1 = open('cell1_only.txt')
 outCell = open('compared.txt', 'w')
 
-#protein = ''
 for line1 in inCell1:
-    for line2 in inCell2:
-      if (line1.split('-'))[0].strip('\n') == (line2.split('-'))[0].strip('\n'):
-        outCell.write('CB660\t' + line1.strip('\n') + '\tG166_1105\t' + line2)
+  inCell2 = open('cell2_only.txt')
+  for line2 in inCell2:
+    if (line1.split('-'))[0].strip('\n') == (line2.split('-'))[0].strip('\n'):
+      outCell.write('CB660\t' + line1.strip('\n') + '\tG166_1105\t' + line2)
+  inCell2.close()
 inCell1.close()
-inCell2.close()
 outCell.close()
